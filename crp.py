f6e2de5d7a2fe492d64ab358efb1bb408138fe5f
Akriti Kapur <akritikapur1@gmail.com>
2018-04-15 12:05:51 -0600

    assigning alreay occupied table functionality added

--- crp.py
+++ crp.py
@@ -16,8 +16,21 @@ def assign_new_table(n):
         return alpha / (n - 1 + alpha)
 
 
-def assign_occupied_table(n, K):
-    pass
+def assign_occupied_table(n, table_people_map):
+    """
+    :param n: number of customers in the restraunt
+    :param table_people_map: people at each table.
+    :return: Probabilties of choosing already assigned table
+    """
+    # this indicates that this is the first customer
+    if not table_people_map:
+        # probability 0 since there are no occupied tables
+        return 0
+
+    Z  = n - 1 + alpha
+    k = np.array(table_people_map)
+    k = k / Z
+    return k
 
 
 def plot_assignments(tot_cus):
@@ -32,6 +45,15 @@ def plot_assignments(tot_cus):
 
     plt.plot(x, y)
     plt.show()
+
+
+def generative_model(tot_cus):
+    table_params = []  # Param for table 1 would be at index 0, 2 at index 1 and so on.
+    table_cus_map = [] # Indicates how many customers occupy each table
+
+    for cus in range(tot_cus):
+        pass
+
 
 if __name__ == '__main__':
     plot_assignments(500)
