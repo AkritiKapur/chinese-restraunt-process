adbad19a396d0262ad79c915937f2990b7d84d55
Akriti Kapur <akritikapur1@gmail.com>
2018-04-15 14:03:46 -0600

    generative model of crp added

--- crp.py
+++ crp.py
@@ -1,7 +1,10 @@
+import random
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 alpha = 0.5  # Concentration parameter
+colors = ['green', 'red', 'blue', 'black', 'orange', 'pink', 'yellow', 'purple']
 
 
 def assign_new_table(n):
@@ -20,17 +23,17 @@ def assign_occupied_table(n, table_people_map):
     """
     :param n: number of customers in the restraunt
     :param table_people_map: people at each table.
-    :return: Probabilties of choosing already assigned table
+    :return: {Integer numpy array} Probabilties of choosing already assigned table
     """
     # this indicates that this is the first customer
     if not table_people_map:
         # probability 0 since there are no occupied tables
-        return 0
+        return [0]
 
-    Z  = n - 1 + alpha
+    Z = n - 1 + alpha
     k = np.array(table_people_map)
     k = k / Z
-    return k
+    return k.tolist()
 
 
 def plot_assignments(tot_cus):
@@ -49,11 +52,44 @@ def plot_assignments(tot_cus):
 
 def generative_model(tot_cus):
     table_params = []  # Param for table 1 would be at index 0, 2 at index 1 and so on.
-    table_cus_map = [] # Indicates how many customers occupy each table
+    table_cus_map = []  # Indicates how many customers occupy each table
+    cur_tables = 0
+    samples = []  # In the form (x-cord, y-cord, color)
 
     for cus in range(tot_cus):
-        pass
+        p_new = assign_new_table(cus + 1)
+        p_occ = assign_occupied_table(cus + 1, table_cus_map)
+        table_p = p_occ + [p_new]
+        if cus == 0:
+            table_p = [p_new]
+
+        table_chosen = np.random.multinomial(n=1, pvals=table_p, size=1)
+
+        table_chosen = np.argmax(table_chosen[0])
+
+        # if new table chosen
+        if table_chosen == cur_tables:
+            # increment current tables
+            cur_tables += 1
+            # One customer added to the new table
+            table_cus_map.append(1)
+            # Sample dish distribution to the new table from distribution G0
+            x = np.random.random_sample()
+            y = np.random.random_sample()
+
+            # Add dish distribution to table
+            table_params.append((x, y))
+
+        # Draw sample dishes from chosen table's dish distribution
+        color = colors[table_chosen]
+        mean = [table_params[table_chosen][0], table_params[table_chosen][1]]
+        covariance = [[0.1, 0], [0, 0.1]]
+        x, y = np.random.multivariate_normal(mean, covariance)
+        samples.append([x, y, color])
+
+    return samples
 
 
 if __name__ == '__main__':
     plot_assignments(500)
+    samples = generative_model(10)
